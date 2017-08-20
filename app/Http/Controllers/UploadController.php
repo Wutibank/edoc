cb0ee928a27f16d6afd5dcadebe33d4dcf4fea51
Wutthichai Panyasung <wutibank@gmail.com>
2017-08-20 13:53:38 +0700

    File Move copy

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -18,9 +18,25 @@ class UploadController extends Controller
        $request->file('image');
       // return $request->image->store('public');
       // return $request->image->extention(); นามสกุลของไฟล์
-    return Storage::putFile('public',$request->file('image'));
+return $request->image->storeAs('public','bank.jpg'); //save with name file
+
+    //return Storage::putFile('public/new',$request->file('image'));
     }else{
         return view('upload.upload');
     }
+    }
+
+    public function show(){
+        //return Storage::allFiles('public');
+        /*if (Storage::makeDirectory('public/make')){
+            return 'Delete';
+        }*/
+       /* $url =  Storage::url('bank.jpg');
+        return "<img src='".$url."'/>"; แสดงงไฟล์*/
+        
+        if (Storage::move('public/bank.jpg','new/bank.jpg')){
+            return 'File Move';
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('upload','UploadController@index');
 Route::post('store','UploadController@store');
+Route::get('show','UploadController@show');
